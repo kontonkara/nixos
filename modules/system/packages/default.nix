b2ae2b5744865211637ac1e20dd3e0d04eac8cb4
Nikita Konton <kontonkara@gmail.com>
2026-08-23 21:16:17 +0300

    fix: sync mcontrolcenter with external power profiles

--- modules/system/packages/default.nix
+++ modules/system/packages/default.nix
@@ -7,6 +7,9 @@
 
 let
   cfg = config.modules.packages;
+  mcontrolcenterWithExternalProfileSync = pkgs.mcontrolcenter.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patches or [ ]) ++ [ ./mcontrolcenter-sync-external-profile.patch ];
+  });
 in
 {
   options = {
@@ -20,7 +23,7 @@ in
   config = lib.mkIf cfg.enable {
     environment = {
       systemPackages = with pkgs; [
-        mcontrolcenter
+        mcontrolcenterWithExternalProfileSync
         binutils
         pciutils
         usbutils
